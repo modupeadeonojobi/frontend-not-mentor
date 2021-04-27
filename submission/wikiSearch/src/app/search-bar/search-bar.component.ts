6f2725a2897bdf8bd95948ecc7f72969f60a5e5f
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2021-04-27 09:02:05 +0100

    Add search-bar component

--- submission/wikiSearch/src/app/search-bar/search-bar.component.ts
+++ submission/wikiSearch/src/app/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchBarComponent implements OnInit {
 
+  @Output() submitted = new EventEmitter<string>();
+  term: string;
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  sumbitForm(event: any) {
+    event.preventDefault();
+    this.submitted.emit(this.term);
   }
 
 }
