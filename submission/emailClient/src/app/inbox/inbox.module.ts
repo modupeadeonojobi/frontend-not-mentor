faba39022827660a10352229f7c2bd5907d9b411
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2021-05-19 06:07:27 +0100

    Add inbox modules

--- submission/emailClient/src/app/inbox/inbox.module.ts
+++ submission/emailClient/src/app/inbox/inbox.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { InboxRoutingModule } from './inbox-routing.module';
 import { InboxHomeComponent } from './inbox-home/inbox-home.component';
+import { EmailCreateComponent } from './email-create/email-create.component';
+import { EmailShowComponent } from './email-show/email-show.component';
+import { EmailIndexComponent } from './email-index/email-index.component';
+import { EmailReplyComponent } from './email-reply/email-reply.component';
 
 
 @NgModule({
-  declarations: [InboxHomeComponent],
+  declarations: [InboxHomeComponent, EmailCreateComponent, EmailShowComponent, EmailIndexComponent, EmailReplyComponent],
   imports: [
     CommonModule,
     InboxRoutingModule
